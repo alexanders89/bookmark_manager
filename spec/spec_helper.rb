5a78b30856e79cd3a0e714ef7b95a79dc1c45b1f
Alex <alex.satur89@gmail.com>
2017-10-24 17:18:44 +0100

    heroku deploy

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,7 +25,6 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
   end
 
-
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
